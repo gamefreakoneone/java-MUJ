e94cfbd502c097bac1ab352b4e6af099353f97cd
gamefreakoneone <amogh@outlook.com>
2020-08-20 22:54:11 +0530

    Upload

---
+++ main.java
@@ -0,0 +1,7 @@
+class A{
+    public static void main(String args[]) {
+        System.out.println("This is a test!!");
+        System.out.print("This is without a new line!");
+        System.out.print("Hello buddy retard!");
+    }
+}
