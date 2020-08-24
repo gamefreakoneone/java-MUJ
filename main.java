50adee6557e74a594d30a129910fd195293b1a23
gamefreakoneone <amogh@outlook.com>
2020-08-24 09:51:45 +0530

    Upload

--- main.java
+++ main.java
@@ -1,7 +1,8 @@
 class A{
     public static void main(String args[]) {
-        System.out.println("This is a test!!");
-        System.out.print("This is without a new line!");
-        System.out.print("Hello buddy retard!");
+        byte b;
+        b=40;
+        b=(byte)(b+1);
+        System.out.println(b);
     }
 }
